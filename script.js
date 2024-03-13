7167a3b8b13df1f38dd79047de86cd4810d872af
SatyaKrishna12 <149576046+SatyaKrishna12@users.noreply.github.com>
2024-03-13 15:54:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,6 @@ let speech = new SpeechSynthesisUtterance();
 let voices = [];
 let voiceselect = document.querySelector("select");
 let selctoption = document.querySelector("option");
-window.speechSynthesis.onvoiceschanged = () => {
-  voices = window.speechSynthesis.getVoices();
-  voices.forEach(
-    (voice, i) => (voiceselect.options[i] = new Option(voice.name, i))
-  );
-  speech.voice = voices[0];
-};
 voiceselect.addEventListener("change", () => {
   speech.voice = voices[voiceselect.value];
 });
@@ -17,3 +10,10 @@ btn.addEventListener("click", () => {
   speech.text = document.querySelector("textarea").value;
   window.speechSynthesis.speak(speech);
 });
+window.speechSynthesis.onvoiceschanged = () => {
+  voices = window.speechSynthesis.getVoices();
+  voices.forEach(
+    (voice, i) => (voiceselect.options[i] = new Option(voice.name, i))
+  );
+  speech.voice = voices[0];
+};
